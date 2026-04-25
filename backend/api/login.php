18733e75b8436a355b5bc4058150cfc071cf0fc2
Farishta Khan <farishtakhan1527@gmail.com>
2026-04-25 12:54:01 +0500

    updated login.php

--- backend/api/login.php
+++ backend/api/login.php
@@ -1,38 +1,3 @@
 <?php
-echo "API is working";
-?>
-<?php
-include("../config/db.php");
-
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-
-    $email = $_POST['email'];
-    $password = $_POST['password'];
-
-    $result = $conn->query("SELECT * FROM users WHERE email='$email'");
-
-    if ($result->num_rows > 0) {
-
-        $user = $result->fetch_assoc();
-
-        if (password_verify($password, $user['password'])) {
-
-            echo json_encode([
-                "status" => "success",
-                "message" => "Login successful",
-                "user" => [
-                    "id" => $user['id'],
-                    "name" => $user['name'],
-                    "email" => $user['email']
-                ]
-            ]);
-
-        } else {
-            echo json_encode(["status" => "error", "message" => "Wrong password"]);
-        }
-
-    } else {
-        echo json_encode(["status" => "error", "message" => "User not found"]);
-    }
-}
+echo "HELLO TEST";
 ?>
